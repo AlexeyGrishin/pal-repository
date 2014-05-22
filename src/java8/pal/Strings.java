ee21bd04acf07457ca0fcee15e68bc04e287cebf
alex <graale@list.ru>
2014-05-22 09:09:40 -0700

    just commit

--- src/java8/pal/Strings.java
+++ src/java8/pal/Strings.java
@@ -1,14 +1,11 @@
 package pal;
 //::imports
 import static pal.Checks.*;
-
 //::module strings
 public class Strings {
-
     public static String truncate(String str, int maxLength) {
         return truncate(str, maxLength, "...");
     }
-
     //::function truncate
     //@@depends isDefined, isPositive
     /**
@@ -23,17 +20,16 @@ public class Strings {
         //@unit ("abc", 3) => "abc"
         //@unit ("abcd", 3) => "a..."
     }
-
-
     //::function truncate
     //@@depends isDefined
     public static String normalizeSpaces(String str) {
         isDefined(str, "str");
         return str.replaceAll("\\s+", " ");
     }
-
-
-
+  //::function truncateLeading
+  public static String truncateLeading(String str, int maxLength, String leading) {
+     return str;
+  }
+    
     //::addbefore
-
 }
